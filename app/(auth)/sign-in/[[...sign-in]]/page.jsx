d9673eb1a123b2a3c97f8f7676700d1952e56dde
Ayush Dwivedi <amayush0818@gmail.com>
2025-05-21 21:44:54 +0530

    Fixed SignIn SignUp page

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,8 +1,10 @@
 import { SignIn } from '@clerk/nextjs'
 import React from 'react'
 
-export const SignInPage = () => {
+const SignInPage = () => {
   return (
     <SignIn />
   )
 }
+
+export default SignInPage
