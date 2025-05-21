04d2364448e47f502406e0bb04e14cdfd1e35b31
Ayush Dwivedi <amayush0818@gmail.com>
2025-05-22 00:52:11 +0530

    Waitlist Page created using CREATE AI

---
+++ app/waitlist/page.jsx
@@ -0,0 +1,19 @@
+
+
+const WaitlistPage = () => {
+    return (
+        <div className="mt-20 fixed w-full">
+            <div className="aspect-video w-full min-h-screen">
+                <iframe 
+                    style={{ width: "100%"}}
+                    height="569"
+                    src="https://vehiclr-waitlist.created.app" 
+                    title="Waiting List Landing Page" 
+                    frameBorder="0"
+                ></iframe>
+            </div>
+        </div>
+    )
+}
+
+export default WaitlistPage
