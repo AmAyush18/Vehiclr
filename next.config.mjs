be8a79fef730c6e0e34dd529cee054ab71996d44
Ayush Dwivedi <amayush0818@gmail.com>
2025-05-25 08:49:39 +0530

    Add Car server action

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        serverComponentsHmrCache: false, // defaults to true
+    },
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "slfbwsacuzszxwchgbpr.supabase.co"
+            }
+        ]
+    },
+
     async headers() {
         return [
             {
